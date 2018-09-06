3b041b933e537cfa2a9f5e7e2cbc50ea932aeb67
山田秀明 <yamadahideaki@yamadahideakinoMacBook-Pro.local>
2018-09-06 18:30:24 +0900

    [finish]2-6

---
+++ 2-6.rb
@@ -0,0 +1,20 @@
+puts 100
+# 出力結果=>100
+puts 100 + 5
+# 出力結果105
+puts "Rubyを学習"
+# 出力結果=>Rubyを学習
+name = "鈴木さん"
+puts "Hello" + name
+# 出力結果=>Hello鈴木さん
+puts "こんにちは#{name}"
+# 出力結果=>こんにちは鈴木さん
+
+name = ["Peal", "Ruby" ,"Python" ,"Swift"]
+puts name[1]
+#　出力結果=>Ruby
+
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+# 出力結果=>185
+
